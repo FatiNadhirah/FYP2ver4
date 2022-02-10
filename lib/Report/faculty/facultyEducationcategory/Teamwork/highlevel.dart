869a03a515f556449092fe465158bca44920d1cd
FatiNadhirah <fatinadhirahbujairimi2805@gmail.com>
2022-02-11 03:28:25 +0800

    add new faculty and year

---
+++ lib/Report/faculty/facultyEducationcategory/Teamwork/highlevel.dart
@@ -0,0 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class TeamHigh extends StatefulWidget {
+  @override
+  _TeamHighState createState() => _TeamHighState();
+}
+
+class _TeamHighState extends State<TeamHigh> {
+  final Stream<QuerySnapshot> _usersStream =
+  FirebaseFirestore.instance.collection('users').where("teamLevel",isEqualTo: "High").where("faculty", isEqualTo: "FSKTM").snapshots();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back,
+            size: 30.0,
+            color: Colors.white,),
+        ),
+        title: Text('FSKTM > High Teamwork'),
+      ),
+      body: StreamBuilder(
+        stream: _usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("something is wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (_, index) {
+                return GestureDetector(
+                  //if tap on the list will go to the data index
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (_) =>
+                    //         UserDetails(docid: snapshot.data!.docs[index]),
+                    //   ),
+                    // );
+                  },
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: 3,
+                          right: 3,
+                        ),
+                        child: ListTile(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                            side: BorderSide(
+                              color: Colors.black,
+                            ),
+                          ),
+                          title: Text(
+                            snapshot.data!.docChanges[index].doc['username'],
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          subtitle: Text(
+                              snapshot.data!.docChanges[index].doc['department'] + "         Year " + snapshot.data!.docChanges[index].doc['year']
+                          ),
+                          trailing: Text(
+                            (snapshot.data!.docChanges[index].doc['teamScore']).toStringAsFixed(0) + "%",
+                            style: TextStyle(
+                                fontSize: 20
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 6,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/Report/year/yearFivecategory/Communication/highlevel.dart
@@ -0,0 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ComHigh extends StatefulWidget {
+  @override
+  _ComHighState createState() => _ComHighState();
+}
+
+class _ComHighState extends State<ComHigh> {
+  final Stream<QuerySnapshot> _usersStream =
+  FirebaseFirestore.instance.collection('users').where("comLevel",isEqualTo: "High").where("year", isEqualTo: "1").snapshots();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back,
+            size: 30.0,
+            color: Colors.white,),
+        ),
+        title: Text('Year 1 > High Communication'),
+      ),
+      body: StreamBuilder(
+        stream: _usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("something is wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (_, index) {
+                return GestureDetector(
+                  //if tap on the list will go to the data index
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (_) =>
+                    //         UserDetails(docid: snapshot.data!.docs[index]),
+                    //   ),
+                    // );
+                  },
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: 3,
+                          right: 3,
+                        ),
+                        child: ListTile(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                            side: BorderSide(
+                              color: Colors.black,
+                            ),
+                          ),
+                          title: Text(
+                            snapshot.data!.docChanges[index].doc['username'],
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          subtitle: Text(
+                              snapshot.data!.docChanges[index].doc['faculty'] + "         " + snapshot.data!.docChanges[index].doc['department']
+                          ),
+                          trailing: Text(
+                            (snapshot.data!.docChanges[index].doc['comScore']).toStringAsFixed(0) + "%",
+                            style: TextStyle(
+                                fontSize: 20
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 6,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
